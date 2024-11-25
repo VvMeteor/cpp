6c813d0cc69c4978c6e0687fcaf62723edff791a
VvMeteor <2665427583@qq.com>
2024-11-25 22:15:32 +0800

    Add files via upload

--- First.cpp
+++ First.cpp
@@ -1,26 +1,64 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-int main()
+//int main()
+//{
+//	cout << "hello world!" << endl;
+//	int a = 10;
+//	cout << "a=" << a << endl;
+//
+//	//单精度float和双精度double，输出小数默认最多6位有效数字
+//	float f1 = 3.1415926f;//编译器默认小数为双精度，加f转化，不加也没问题，float会起作用
+//	cout << "f1=" << f1 << endl;
+//
+//	//科学计数法
+//	float b = 3e2;//等效于3*10^2=300
+//	cout << "b=" << b << endl;
+//
+//	//常用的转义字符
+//	cout << "hehe\n";
+//	cout << "hahaha\taaaaa" << endl;//\t加上前面字符串一共8个空格位，用于水平制表
+//	cout << "haha\tbbbbb" << endl;
+//	cout << "hahaha\tcccc" << endl;
+//	cout << "\\" << endl;
+//
+//	return 0;
+//}
+
+//输出字符串
+//int main()
+//{
+//	//c语言风格
+//	char str[] = "hello world!";
+//	cout << str << endl;
+//	//c++分格,需要引入头文件<string>
+//	string str1 = "hello world!";
+//	cout << str1 << endl;
+//	return 0;
+//}
+
+//布尔类型
+//int main()
+//{
+//	bool flag = true;
+//	cout << flag << endl;
+//	flag = false;
+//	cout << flag << endl;
+//	cout << "bool类型的大小是：" << sizeof(bool) << endl;
+//	return 0;
+//}
+
+//输入数据
+int  main()
 {
-	cout << "hello world!" << endl;
-	int a = 10;
-	cout << "a=" << a << endl;
+	int a = 0;
+	cout << "请输入a的值：" << endl;
+	cin >> a;
+	cout << "变量a的值为：" << a<<endl;
 
-	//单精度float和双精度double，输出小数默认最多6位有效数字
-	float f1 = 3.1415926f;//编译器默认小数为双精度，加f转化，不加也没问题，float会起作用
-	cout << "f1=" << f1 << endl;
-
-	//科学计数法
-	float b = 3e2;//等效于3*10^2=300
-	cout << "b=" << b << endl;
-
-	//常用的转义字符
-	cout << "hehe\n";
-	cout << "hahaha\taaaaa" << endl;//\t加上前面字符串一共8个空格位，用于水平制表
-	cout << "haha\tbbbbb" << endl;
-	cout << "hahaha\tcccc" << endl;
-	cout << "\\" << endl;
-
+	string str = "";
+	cin >> str;
+	cout << str << endl;
 	return 0;
 }
