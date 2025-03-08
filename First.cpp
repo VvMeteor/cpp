3dd1c6ff6a243c28920566398e09d45077e62915
VvMeteor <2665427583@qq.com>
2025-03-08 20:08:53 +0800

    Add files via upload

--- First.cpp
+++ First.cpp
@@ -3831,20 +3831,82 @@ using namespace std;
 //string(const string& str) :
 //string(int n, char c) :
 
-void test()
-{
-	string s1;
-	string s2("hello world");
-	string s3(s2);
-	string s4(10, 'a');
-	cout << s2 << endl;
-	cout << s3 << endl;
-	cout << s4 << endl;
+//void test()
+//{
+//	string s1;
+//	string s2("hello world");
+//	string s3(s2);
+//	string s4(10, 'a');
+//	cout << s2 << endl;
+//	cout << s3 << endl;
+//	cout << s4 << endl;
+//
+//}
+//int main()
+//{
+//	test();
+//	system("pause");
+//	return 0;
+//}
 
-}
+//string的赋值操作
+//string& operator=(const char* s);//char*类型字符串 赋值给当前的字符串
+//string& operator=(const string& s);//把字符串s赋给当前的字符串
+//string& operator=(char c);//字符赋值给当前的字符串
+//string& assign(const char* s);//把字符串s赋给当前的字符串
+//string& assign(const char* s, int n); //把字符串s的前n个字符赋给当前的字符串
+//string& assign(const string& s);//把字符串s赋给当前字符串
+//string& assign(int n, char c);//用n个字符c赋给当前字符串
+
+//int main()
+//{
+//	string str1;
+//	str1 = "hello";
+//	cout << str1 << endl;
+//
+//	string str2;
+//	str2 = str1;
+//	cout << str2 << endl;
+//
+//	string str3;
+//	str3 = 'a';
+//	cout << str3 << endl;
+//
+//	string str4;
+//	str4.assign("hello");
+//	cout << str4 << endl;
+//
+//	string str5;
+//	str5.assign("hello world",5);
+//	cout << str5 << endl;
+//
+//	string str6;
+//	str6.assign(str5);
+//	cout << str6 << endl;
+//
+//	string str7;
+//	str7.assign(5, 'a');
+//	cout << str7 << endl;
+//	
+//	system("pause");
+//	return 0;
+//}
+
+//string中的查找和替代
+
 int main()
 {
-	test();
+	string str = "helloworldlo";
+	int op = str.find("lo");//找到就返回下标，找不到就返回-1
+	cout << op << endl;
+
+	//rfind和find的区别在于find是从左至右找，rfind是从右至左
+	int op1 = str.rfind("lo");
+	cout << op1 << endl;
+
+	//从下标为1的位置开始的三个字符替换为1111
+	str.replace(1, 3, "1111");
+
 	system("pause");
 	return 0;
 }
